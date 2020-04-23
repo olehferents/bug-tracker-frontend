1a5014f66e8f3390303833ffa815c48f7b9d2100
olehferents <olegferents1@gmail.com>
2020-04-23 19:37:14 +0300

    Render form depends on button click

--- src/components/HomePage/WelcomeBlock/Welcome.js
+++ src/components/HomePage/WelcomeBlock/Welcome.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Box from '@material-ui/core/Box';
 import Fade from '@material-ui/core/Fade';
 import Button from '@material-ui/core/Button';
 import {makeStyles} from '@material-ui/core/styles';
+import Modal from 'react-modal';
+import SignUp from '../Auth/SignUp';
+import SignIn from '../Auth/SignIn';
 
 const useStyles = makeStyles({
     root: {
@@ -35,27 +38,71 @@ const useStyles = makeStyles({
     },
 });
 
+const modalCustomStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+
 const Welcome = () => {
     const styles = useStyles();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalType, setModalType] = useState(null);
+
+    const changeModal = (type) => {
+        setModalType(type);
+        openModal();
+    };
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div>
-            <Box className={styles.root}>
-                <Box>
-                    <Fade in={true} timeout={4000}>
-                        <span className={styles.title}>Bug Tracker System <br/>
-                            <span className={styles.contentText}>Choose one of the options below</span>
-                        </span>
-                    </Fade>
+            <Fade in={true} timeout={4000}>
+                <Box className={styles.root}>
+                    <Box>
+                    <span className={styles.title}>Bug Tracker System <br/>
+                        <span className={styles.contentText}>Choose one of the options below</span>
+                    </span>
+                    </Box>
+                    <Box className={styles.authButtons}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            size="large"
+                            onClick={() => changeModal('signIn')}
+                        >
+                            Sign In
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            size="large"
+                            onClick={() => changeModal('signUp')}
+                        >
+                            Sign Up
+                        </Button>
+                    </Box>
                 </Box>
-                <Box className={styles.authButtons}>
-                    <Button variant="contained" color="primary" size="large">
-                        Sign In
-                    </Button>
-                    <Button variant="contained" color="primary" size="large">
-                        Sign Up
-                    </Button>
-                </Box>
-            </Box>
+            </Fade>
+            <Modal
+                isOpen={isModalOpen}
+                onRequestClose={closeModal}
+                style={modalCustomStyles}
+            >
+                {modalType === 'signUp' ? <SignUp/> : <SignIn/>}
+            </Modal>
         </div>
     )
 };
