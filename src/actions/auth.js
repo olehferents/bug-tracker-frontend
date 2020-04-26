c245c7f7b0ff5e2634c9debcadc00d3629af8fed
olehferents <olegferenc1@gmail.com>
2020-04-26 18:49:10 +0300

    Refactore action types

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,8 +3,6 @@ export const moduleName = 'auth';
 export const SUCCESS = '_SUCCESS';
 export const FAILED = '_FAILED';
 
-export const SIGN_UP = 'SIGN_UP';
-export const SIGN_IN = 'SIGN_IN';
-export const LOG_OUT = 'LOG_OUT';
-
-
+export const SIGN_UP = `${moduleName}/SIGN_UP`;
+export const SIGN_IN = `${moduleName}/SIGN_IN`;
+export const LOG_OUT = `${moduleName}/LOG_OUT`;
