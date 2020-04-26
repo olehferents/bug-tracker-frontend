180824e91d1c012bf1d7ff93975621dd0dde74ac
olehferents <olegferenc1@gmail.com>
2020-04-26 18:49:36 +0300

    Change form helper text

--- src/const/auth.js
+++ src/const/auth.js
@@ -1,6 +1,7 @@
-export const formErrorText = {
-    fname: 'Wrong first name format!',
-    lname: 'Wrong last name format!',
-    email: 'Wrong email format!',
-    password: 'Wrong password format!',
+export const formHelperText = {
+    fname: 'Wrong first name format',
+    lname: 'Wrong last name format',
+    email: 'Wrong email format',
+    password: 'Password must be between 6 to 20 characters, ' +
+        'contain at least one numeric digit, one uppercase and one lowercase letter',
 };
