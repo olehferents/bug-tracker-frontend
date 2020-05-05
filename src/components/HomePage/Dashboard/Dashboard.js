d80840403187372d2e6c5bbb76b01ae167195c10
olehferents <olegferenc1@gmail.com>
2020-05-05 20:03:10 +0300

    Add basic sidebar and main block

--- src/components/HomePage/Dashboard/Dashboard.js
+++ src/components/HomePage/Dashboard/Dashboard.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import Projects from './ProjectsPage/Projects';
+import Dialogs from './DialogsPage/Dialogs';
+import Account from './AccountPage/Account';
+import Settings from './SettingsPage/Settings';
 
 const Dashboard = () => {
     return (
-        <div>
-            <Route path='/projects'/>
-            <Route path='/dialogs'/>
-            <Route path='/account'/>
-            <Route path='/settings'/>
-        </div>
+        <>
+            <Route path='/projects' component={Projects}/>
+            <Route path='/dialogs' component={Dialogs}/>
+            <Route path='/account' component={Account}/>
+            <Route path='/settings' component={Settings}/>
+        </>
     )
 };
 
