8f4dc6d5f263df70bee80d5950a3d128fc3c30fc
olehferents <olegferents1@gmail.com>
2020-05-06 16:44:22 +0300

    Moved Modal.setAppElement to HomePage component

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {getIsSignedIn} from '../../reducers/auth';
 import Welcome from './WelcomeBlock/Welcome';
 import Dashboard from './Dashboard/Dashboard';
 import {ACCESS_TOKEN} from '../../const/auth';
 import {SIGN_IN, SUCCESS} from '../../actions/auth';
+import Modal from 'react-modal';
 
 const HomePage = () => {
     const isSignedIn = useSelector(state => getIsSignedIn(state));
@@ -14,6 +15,11 @@ const HomePage = () => {
     if (token) {
         dispatch({type: SIGN_IN + SUCCESS, payload: true});
     }
+
+    useEffect(() => {
+        Modal.setAppElement('body');
+    }, []);
+
     return (
         <div>
             {isSignedIn ? <Dashboard/> : <Welcome/>}
