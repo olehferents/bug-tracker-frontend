05b562668e2e63a59166393e3f58c696cc5537cd
olehferents <olegferents1@gmail.com>
2020-05-09 21:01:29 +0300

    Add component that contain user's project list

---
+++ src/components/HomePage/Dashboard/ProjectsPage/ProjectsListSideBar.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/styles';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import {useDispatch, useSelector} from 'react-redux';
+import {getUserProjects} from '../../../../reducers/project';
+import {CHANGE_CURRENT_PROJECT} from '../../../../actions/project';
+
+const useStyles = makeStyles({
+    sideBar: {
+        display: 'flex',
+    },
+    projectsList: {
+        display: 'flex',
+        width: '100%',
+        maxWidth: 230,
+        overflow: 'auto',
+        maxHeight: `${window.innerHeight}px`,
+        height: `${window.innerHeight - 105}px`,
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        marginTop: '10px',
+        marginLeft: '5px',
+        padding: '10px',
+    },
+    ul: {
+        backgroundColor: 'white',
+        padding: 0,
+        pointer: 'cursor',
+        width: '100%',
+    },
+    listItem: {
+        '& span': {
+            fontSize: '14px',
+            fontWeight: 'bold',
+        },
+    },
+});
+
+const ProjectsListSideBar = () => {
+    const styles = useStyles();
+    const dispatch = useDispatch();
+
+    const projects = useSelector(state => getUserProjects(state));
+
+    const changeProject = (project) => {
+        dispatch({type: CHANGE_CURRENT_PROJECT, payload: project});
+    };
+
+    return (
+        <Box className={styles.sideBar}>
+            <Box boxShadow={3} className={styles.projectsList}>
+                <Typography variant="h6" gutterBottom>Projects list</Typography>
+                <List>
+                    <ul className={styles.ul}>
+                        {projects.map(item => (
+                            <ListItem button className={styles.listItem} onClick={() => changeProject(item)}>
+                                <ListItemText primary={`Item ${item.name}`}/>
+                            </ListItem>
+                        ))}
+                    </ul>
+                </List>
+            </Box>
+        </Box>
+    )
+};
+
+export default ProjectsListSideBar;
