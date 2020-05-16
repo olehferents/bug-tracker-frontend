b968575b64df17352f1a7d72212219d4604777da
olehferents <olegferents1@gmail.com>
2020-05-16 11:33:00 +0300

    Tables columns

---
+++ src/const/columns.js
@@ -0,0 +1,38 @@
+export const issues = [
+    {
+        id: 'id',
+        label: 'Id',
+        minWidth: 100
+    },
+    {
+        id: 'priority',
+        label: 'Priority',
+        minWidth: 120
+    },
+    {
+        id: 'category',
+        label: 'Category',
+        minWidth: 170
+    },
+    {
+        id: 'status',
+        label: 'Status',
+        minWidth: 170
+    },
+    {
+        id: 'updatedAt',
+        label: 'Updated at',
+        minWidth: 170
+    },
+    {
+        id: 'description',
+        label: 'Description',
+        minWidth: 100
+    },
+];
+
+export const membersColumns = [
+    {title: 'First name', field: 'firstName'},
+    {title: 'Last name', field: 'lastName'},
+    {title: 'Email', field: 'email'},
+];
